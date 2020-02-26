80b9c480f1b85fe51fae7b5262130b966c92f3a7
Amar-A <amaramee11@gmail.com>
2020-02-26 21:16:01 +0100

    Jos par promena

--- Program.cs
+++ Program.cs
@@ -11,11 +11,10 @@ namespace Zadaci_3
     {
         static void Main()
         {
-            char unos_korisnika = ' ';
             do
             {
                 Meni();
-                unos_korisnika = Console.ReadKey().KeyChar;
+                char unos_korisnika = Console.ReadKey().KeyChar;
                 if (Imenik.Osobe.Count == 0 && unos_korisnika != '1')
                 {
                     if (unos_korisnika == '0')
@@ -40,12 +39,15 @@ namespace Zadaci_3
                     case '4':
                         Izmena();
                         break;
+                    case '0':
+                        Environment.Exit(0);
+                        break;
                     default:
                         Console.WriteLine("\nGreska u unosu!");
                         break;
                 }
 
-            } while (unos_korisnika != '0');
+            } while (true);
         }
 
         static void Meni()
@@ -78,21 +80,21 @@ namespace Zadaci_3
             for (int i = 0; i < Imenik.Osobe.Count; i++)
             {
                 Console.WriteLine($"{i + 1}.\t{Imenik.Osobe[i].ime}\t{Imenik.Osobe[i].prezime}\t  {Imenik.Osobe[i].broj}");
-            } 
+            }
         }
 
         static void Brisanje()
         {
-            
+            Prikaz();
             Console.Write("\nOdaberite kontakt koji zelite da obrisete > ");
             proveraUnosa(out int obrisati);
             Imenik.Osobe.RemoveAt(obrisati - 1);
         }
-        static void Izmena() 
+        static void Izmena()
         {
             Prikaz();
             Console.Write("Odaberite kontakt koji zelite da izmenite > ");
-            proveraUnosa(out int promeniti);           
+            proveraUnosa(out int promeniti);
             string promena = "";
             char unos = ' ';
             while (promena != "0")
@@ -104,18 +106,18 @@ namespace Zadaci_3
                 {
                     case '1':
                         Console.Write("\nNovo ime > ");
-                        string novo_ime = Console.ReadLine();
+                        string novo_ime = ProveraSpace();
                         Imenik.Osobe[promeniti - 1].ime = novo_ime;
                         break;
                     case '2':
                         Console.Write("\nNovo prezime > ");
-                        string novo_prezime = Console.ReadLine();
-                        Imenik.Osobe[promeniti - 1].prezime = novo_prezime;
+                        string novo_prezime = ProveraSpace();
+                        Imenik.Osobe[promeniti - 1].prezime =novo_prezime;
                         break;
                     case '3':
                         Console.Write("\nNovi broj > ");
                         string novi_broj = Console.ReadLine();
-                        if (Imenik.Provera(novi_broj, out _)  ==  true)
+                        if (Imenik.Provera(novi_broj, out _) == true)
                         {
                             Console.WriteLine("Vec postojeci broj!");
                         }
@@ -130,14 +132,14 @@ namespace Zadaci_3
                     default:
                         Console.WriteLine("\nGreska u unosu !");
                         break;
-                }    
+                }
             }
         }
 
         static void proveraUnosa(out int unos) //Metoda proverava da korisnik nije uneo pogresne vrednosti puput stringa, 
         {                                      //i proverava velicinu broja da nije jednaka nuli ili da broj nije veci od ponudjenih opcija.
             bool provera = int.TryParse(Console.ReadLine(), out unos);
-            
+
             while (provera == false || unos > Imenik.Osobe.Count || unos <= 0)
             {
                 if (provera == false)
@@ -150,10 +152,19 @@ namespace Zadaci_3
                     Console.Write("\nGreska ne postojeci kontakt! > ");
                     provera = int.TryParse(Console.ReadLine(), out unos);
                 }
-
             }
         }
+        static string ProveraSpace()
+        {
+            string unos = Console.ReadLine();
+            while (unos.Contains(" "))
+            {
+                Console.Write("Greska! Unos ne sme da poseduje prazan prostor ! > ");
+                unos = Console.ReadLine();
+            }
+            return unos;
 
+        }
         static string ProveraBroja(string unos) //Ova metoda proverava da li je korisnik uneo u polju koje je namenjeno za broj
         {                                      //numericku vrednost. 
             bool check = Regex.IsMatch(unos, @"^[0-9]+$");
